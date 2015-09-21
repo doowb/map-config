c5c0703c0995a7331628f0239318a215a997aad0
Brian Woodward <brian.woodward@gmail.com>
2015-09-21 19:28:45 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * map-config <https://github.com/doowb/map-config>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
