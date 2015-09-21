e6677cb08720785aea6c5cee99f615b98fe1fcb7
Brian Woodward <brian.woodward@gmail.com>
2015-09-21 19:37:42 -0400

    test file

---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * map-config <https://github.com/doowb/map-config>
+ *
+ * Copyright (c) 2015 .
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+/* deps:mocha */
+var assert = require('assert');
+var should = require('should');
+var MapConfig = require('./');
+
+describe('map-config', function () {
+  it('should:', function () {
+  });
+});
