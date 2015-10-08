13cd5dfe8d8dc99387285255e6e1c181ddad42a0
Brian Woodward <brian.woodward@gmail.com>
2015-10-08 14:36:10 -0400

    code coverage

--- test.js
+++ test.js
@@ -15,12 +15,27 @@ var MapConfig = require('./');
 describe('map-config', function () {
   it('should pass a config and the app instance to mapped methods', function () {
     var called = false;
-    var map = {foo: 'bar'};
+    var map = {foo: 'bar', baz: 'bang'};
     var app = {bar: function (config, _app) {
       called = true;
       assert.deepEqual(config, {baz: 'beep'});
       assert.deepEqual(app, _app);
     }};
+    var config = {foo: {baz: 'beep'}};
+    var mapper = new MapConfig(map, app);
+    mapper.process(config);
+    assert(called);
+  });
+
+  it('should pass a config and the app instance to mapped functions', function () {
+    var called = false;
+    var map = {foo: function (config, _app) {
+      called = true;
+      assert.deepEqual(config, {baz: 'beep'});
+      assert.deepEqual(app, _app);
+    }};
+
+    var app = {beep: 'boop'};
     var config = {foo: {baz: 'beep'}};
     var mapper = new MapConfig(map, app);
     mapper.process(config);
@@ -42,5 +57,21 @@ describe('map-config', function () {
     mapper.process(config);
 
     assert.deepEqual(output, ['bar beep']);
+  });
+
+  it('should not map anything when config is empty', function () {
+    var output = [];
+    var map = {foo: 'bar'};
+
+    var app = {
+      bar: function (config) {
+        output.push('bar ' + config.baz);
+      }
+    };
+
+    var mapper = new MapConfig(map, app);
+    mapper.process();
+
+    assert.deepEqual(output, []);
   });
 });
